a3422f1394cca0332a39aba58a071f97db76da8f
Codou Gueye <codoumbakhe@gmail.com>
2025-07-23 14:37:59 -0600

    Initial Commit

---
+++ Interactive UI/Interactive_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Interactive_UIApp.swift
+//  Interactive UI
+//
+//  Created by Scholar on 7/23/25.
+//
+
+import SwiftUI
+
+@main
+struct Interactive_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
